08d0da52f87c567c927ae489275968123658ed4f
Pablo Frias <frias.pablo@gmail.com>
2018-01-23 15:15:28 -0300

    Update bundle

--- dist/bundle.js
+++ dist/bundle.js
@@ -261,7 +261,7 @@ var dataArray = new Uint8Array(3);
 dataArray[0] = 17;
 dataArray[1] = 34;
 dataArray[2] = 1;
-exports.bytesTakeOff = bytesTakeOff;
+exports.bytesTakeOff = dataArray;
 
 
 /***/ })
