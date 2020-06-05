c18ea1f3f924b462572c805d61333ad56e1f0289
Pablo Frias <frias.pablo@gmail.com>
2020-06-05 14:06:31 -0700

    Add rolling certs generator

---
+++ rolling_certs/precache-manifest.1534a77a6a4b3f104d2725161d0e884a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1820fb1955a9bc99c8f3425629aff7e",
+    "url": "/rolling_certs/index.html"
+  },
+  {
+    "revision": "5b732539f3f762574985",
+    "url": "/rolling_certs/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5e715d886c6bb09af42e",
+    "url": "/rolling_certs/static/css/main.074b7fff.chunk.css"
+  },
+  {
+    "revision": "5b732539f3f762574985",
+    "url": "/rolling_certs/static/js/2.1f61c2d2.chunk.js"
+  },
+  {
+    "revision": "57c4e46f361f4b08b02107c38c12b946",
+    "url": "/rolling_certs/static/js/2.1f61c2d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e715d886c6bb09af42e",
+    "url": "/rolling_certs/static/js/main.11a2366c.chunk.js"
+  },
+  {
+    "revision": "93696b6248c123e7036f",
+    "url": "/rolling_certs/static/js/runtime-main.fac496c8.js"
+  },
+  {
+    "revision": "169d8989874ab76ce8d03b178cade738",
+    "url": "/rolling_certs/static/media/CertificadoFullstack1.169d8989.png"
+  },
+  {
+    "revision": "c9f309b3d47969ecac64a77a6c672594",
+    "url": "/rolling_certs/static/media/Nexa-Bold.c9f309b3.otf"
+  },
+  {
+    "revision": "12108809f49c49bcdc126dcecc938761",
+    "url": "/rolling_certs/static/media/Nexa-Light.12108809.otf"
+  },
+  {
+    "revision": "08bc2dc1afd6f4fda0cc026a60fb01e2",
+    "url": "/rolling_certs/static/media/a4vertical.08bc2dc1.jpg"
+  },
+  {
+    "revision": "5f98876a00f14f996a3bf28be1566241",
+    "url": "/rolling_certs/static/media/futura-bold.5f98876a.ttf"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/rolling_certs/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "c89dadf5182187eccdbdcac98e831b63",
+    "url": "/rolling_certs/static/media/rollingcode-logo.c89dadf5.png"
+  }
+]);
